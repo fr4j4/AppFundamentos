8f0931de7c5f7b2faa77b02385631df103cc6b26
fr4j4 <fgonz@outlook.com>
2017-05-12 09:23:44 +0000

    db

--- AppFundamentos/settings.py
+++ AppFundamentos/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'AppFundamentos.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        #'read_default_file': 'config.cnf',
+        'HOST': 'localhost',
+        'NAME' : 'app_fundamentos',
+        'USER' : 'root',
+        'PASSWORD' : 'root',    
     }
 }
 
