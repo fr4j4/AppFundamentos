256a4f371b4483aed54eec2371530581576c0ed8
fr4j4 <fgonz@outlook.com>
2017-05-12 23:04:32 +0000

    rutas basicas creadas

--- common/urls.py
+++ common/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     
     url(r'^academicos/registrar/$', views.registro_academicos, name='acad_reg'),
-    url(r'^academicos/editar/(?P<id>\d+)/$', views.editar_academicos, name='acad_edit'),
+    url(r'^academicos/(?P<id>\d+)/editar/$', views.editar_academicos, name='acad_edit'),
     
     url(r'^asignaturas/registrar/$', views.registro_asignaturas, name='asig_reg'),
-    url(r'^asignaturas/editar/(?P<id>\d+)/$', views.editar_asignaturas, name='asig_edit'),
+    url(r'^asignaturas/(?P<id>\d+)/editar/$', views.editar_asignaturas, name='asig_edit'),
 
     url(r'^academicos/(?P<id_academico>\d+)/cargas/registrar/$', views.registro_cargas, name='carg_reg'),
 	url(r'^academicos/(?P<id_academico>\d+)/cargas/editar/$', views.editar_cargas, name='carg_edit'),
