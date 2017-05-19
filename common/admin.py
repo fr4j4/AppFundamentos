6d6aff436526f2b387a3ef5c43cd0db14ce3dc37
fr4j4 <fgonz@outlook.com>
2017-05-19 00:43:27 +0000

    agregar/modificar cosas

--- common/admin.py
+++ common/admin.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from .models import * 
+
+admin.site.register(Asignatura)
+admin.site.register(Academico)
+admin.site.register(Carga)
--- common/models.py
+++ common/models.py
@@ -2,5 +2,25 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
 # Create your models here.
+
+class Asignatura(models.Model):
+	nombre=models.CharField(max_length=200)
+	codigo=models.CharField(max_length=200)
+
+class Academico(models.Model):
+	nombre=models.CharField(max_length=200)
+	apellido=models.CharField(max_length=200)
+	rut=models.CharField(max_length=20)
+	asignaturas=models.ManyToManyField(Asignatura)
+
+
+class Carga(models.Model):
+	nombre=models.CharField(max_length=200)
+	apellido=models.CharField(max_length=200)
+	rut=models.CharField(max_length=20)
+	academco = models.ForeignKey(Academico, on_delete=models.CASCADE)
+
+
+
+
