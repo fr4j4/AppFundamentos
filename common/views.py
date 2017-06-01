757e74ece237bff583bdc187c7dc7c61d6ec30a1
EKochG <elard.koch@outlook.es>
2017-05-31 20:08:29 -0400

    agregado formulario academicos

--- common/views.py
+++ common/views.py
@@ -10,15 +10,20 @@ from .models import *
 def index(request):
     return render(request,'common/index.html')
 
-def index_cargas(request):
-	return render(request,'common/cargas/index.html')
-
 def index_academicos(request):
 	academicos=Academico.objects.all();
 	context={
 		'academicos':academicos,
 	}
 	return render(request,'common/academicos/index.html',context)
+
+def registro_academicos(request):
+	return render(request,'common/academicos/registrar.html')
+
+def editar_academicos(request,id):
+	return HttpResponse("Editar academicos id_academico={0}".format(id))
+
+################################################################################
 
 def index_asignaturas(request):
 	asignaturas=Asignatura.objects.all();
@@ -27,24 +32,21 @@ def index_asignaturas(request):
 	}
 	return render(request,'common/asignaturas/index.html',context)
 
+def registro_asignaturas(request):
+	return render(request,'common/asignaturas/new.html')
 
-def registro_academicos(request):
-	return HttpResponse("Registro academicos")
+def editar_asignaturas(request,id):
+	return HttpResponse("Editar asignaturas id_asignatura={0}".format(id))
+
+################################################################################
+
+def index_cargas(request):
+	return render(request,'common/cargas/index.html')
 
 def registro_cargas(request,id_academico):
 	return HttpResponse("Registro cargas academico_id={0}".format(id_academico))
 
-
-def registro_asignaturas(request):
-	return render(request,'common/asignaturas/new.html')
-
-
-def editar_academicos(request,id):
-	return HttpResponse("Editar academicos id_academico={0}".format(id))
-
-
 def editar_cargas(request,id_academico):
 	return HttpResponse("Editar cargas id_academico ={0}".format(id_academico))
 
-def editar_asignaturas(request,id):
-	return HttpResponse("Editar asignaturas id_asignatura={0}".format(id))
+
